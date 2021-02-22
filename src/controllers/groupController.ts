6c2f62d433384bebcb3580ef000933c266802c36
data-miner00 <data-miner00@users.noreply.github.com>
2021-02-23 02:12:39 +0800

    feat: Added gantt logics

--- src/controllers/groupController.ts
+++ src/controllers/groupController.ts
@@ -11,7 +11,7 @@ export const addGroup = async (
   try {
     const data = req.body;
     await firestore.collection("groups").doc().set(data);
-    res.send("Record saved");
+    res.status(200).send("Record saved");
   } catch (error) {
     res.status(400).send(error.message);
   }
@@ -24,15 +24,90 @@ export const getGroup = async (
 ) => {
   try {
     const groupId = req.params.id;
-    const group = await firestore.collection("groups").doc(groupId);
-    const data = await group.get();
+    const groupRef = firestore.collection("groups").doc(groupId);
+    const groupSnapshot = await groupRef.get();
+    const groupData = groupSnapshot.data();
 
-    if (data.exists) {
-      res.status(200).send(data.data());
+    if (groupSnapshot.exists) {
+      const ganttRef = firestore
+        .collection("groups")
+        .doc(groupId)
+        .collection("gantts");
+      const ganttSnapshot = await ganttRef.get();
+
+      if (ganttSnapshot.docs.length > 0) {
+        const ganttData: Array<Object> = [];
+
+        ganttSnapshot.forEach((doc) => {
+          ganttData.push({
+            id: doc.id,
+            ...doc.data(),
+          });
+        });
+
+        const data = {
+          ...groupData,
+          gantts: ganttData,
+        };
+
+        res.status(200).send(data);
+      } else {
+        res.status(200).send(groupData);
+      }
     } else {
       res.status(404).send("Data not found");
     }
   } catch (error) {
     res.status(400).send(error.message);
+  }
+};
+
+export const getAllGantt = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const groupId = req.params.id;
+    const ganttRef = firestore
+      .collection("groups")
+      .doc(groupId)
+      .collection("gantts");
+    const ganttSnapshot = await ganttRef.get();
+
+    const ganttData: Array<Object> = [];
+
+    ganttSnapshot.forEach((doc) => {
+      ganttData.push({
+        id: doc.id,
+        ...doc.data(),
+      });
+    });
+
+    res.status(200).send(ganttData);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const addGantt = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const groupId = req.params.id;
+    const gantt = req.body;
+
+    await firestore
+      .collection("groups")
+      .doc(groupId)
+      .collection("gantts")
+      .doc()
+      .set(gantt);
+
+    res.status(200).send("Created");
+  } catch (error) {
+    console.error(error);
   }
 };
