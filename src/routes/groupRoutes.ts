8a211951e8a4152e848e4946829f7d4089e609f3
data-miner00 <data-miner00@users.noreply.github.com>
2021-03-03 23:15:50 +0800

    feat: Added gantt route

--- src/routes/groupRoutes.ts
+++ src/routes/groupRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getGroup } from "../controllers/groupController";
+import { getGroup, getAllGantt } from "../controllers/groupController";
 
 const router: Router = Router();
 
 router.get("/group/:id", getGroup);
+router.get("/group/:id/gantt", getAllGantt);
 
 export default {
   routes: router,
