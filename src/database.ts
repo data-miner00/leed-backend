d4912038c6e0a71168ad18683794f9f65c58a29e
data-miner00 <data-miner00@users.noreply.github.com>
2021-03-03 23:14:28 +0800

    feat: Added 2 more object export

--- src/database.ts
+++ src/database.ts
@@ -2,3 +2,7 @@ import firebase from "firebase";
 import config from "./config";
 
 export const db = firebase.initializeApp(config.firebaseConfig);
+
+export const Timestamp = firebase.firestore.Timestamp;
+
+export const FieldPath = firebase.firestore.FieldPath;
