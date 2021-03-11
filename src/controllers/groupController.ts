9ed38a139f6a4031544303920b340927efb8cad1
data-miner00 <data-miner00@users.noreply.github.com>
2021-03-11 17:34:07 +0800

    feat: Added more functionalities for booking

--- src/controllers/groupController.ts
+++ src/controllers/groupController.ts
@@ -1,5 +1,7 @@
-import { db as firebase } from "../database";
+import { db as firebase, FieldPath, FieldValue } from "../database";
 import { Request, Response, NextFunction } from "express";
+import { generate } from "short-uuid";
+import { bookingAlgorithm } from "../utils";
 
 const firestore = firebase.firestore();
 
@@ -10,7 +12,26 @@ export const addGroup = async (
 ) => {
   try {
     const data = req.body;
-    await firestore.collection("groups").doc().set(data);
+    const creatorId = req.body.id;
+    const newGroupId = generate();
+    await firestore.collection("groups").doc(newGroupId).set(data);
+
+    const creatorRef = firestore.collection("students").doc(creatorId);
+    const creatorData = (await creatorRef.get()).data();
+    if (creatorData!.hasOwnProperty("groupsId"))
+      await firestore
+        .collection("students")
+        .doc(creatorId)
+        .update({
+          groupsId: [newGroupId, ...creatorData!.groupsId],
+        });
+    else
+      await firestore
+        .collection("students")
+        .doc(creatorId)
+        .update({
+          groupsId: [newGroupId],
+        });
     res.status(200).send("Group created");
   } catch (error) {
     res.status(400).send(error.message);
@@ -28,35 +49,7 @@ export const getGroup = async (
     const groupSnapshot = await groupRef.get();
     const groupData = groupSnapshot.data();
 
-    if (groupSnapshot.exists) {
-      const ganttRef = firestore
-        .collection("groups")
-        .doc(groupId)
-        .collection("gantts");
-      const ganttSnapshot = await ganttRef.get();
-
-      if (ganttSnapshot.docs.length > 0) {
-        const ganttData: Array<Object> = [];
-
-        ganttSnapshot.forEach((doc) => {
-          ganttData.push({
-            id: doc.id,
-            ...doc.data(),
-          });
-        });
-
-        const data = {
-          ...groupData,
-          gantts: ganttData,
-        };
-
-        res.status(200).send(data);
-      } else {
-        res.status(200).send(groupData);
-      }
-    } else {
-      res.status(404).send("Data not found");
-    }
+    res.status(200).send(groupData);
   } catch (error) {
     res.status(400).send(error.message);
   }
@@ -72,7 +65,8 @@ export const getAllGantt = async (
     const ganttRef = firestore
       .collection("groups")
       .doc(groupId)
-      .collection("gantts");
+      .collection("gantts")
+      .orderBy("createdAt", "asc");
     const ganttSnapshot = await ganttRef.get();
 
     const ganttData: Array<Object> = [];
@@ -86,7 +80,7 @@ export const getAllGantt = async (
 
     res.status(200).send(ganttData);
   } catch (error) {
-    console.error(error);
+    res.status(400).send(error.message);
   }
 };
 
@@ -96,8 +90,9 @@ export const addGantt = async (
   next: NextFunction
 ) => {
   try {
-    const groupId = req.params.id;
+    const groupId = req.body.id;
     const gantt = req.body;
+    gantt.createdAt = FieldValue.serverTimestamp();
 
     await firestore
       .collection("groups")
@@ -108,6 +103,140 @@ export const addGantt = async (
 
     res.status(200).send("Created");
   } catch (error) {
-    console.error(error);
+    res.status(400).send(error.message);
+  }
+};
+
+export const getGroupMembers = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const groupId = req.params.id;
+    const groupRef = firestore.collection("groups").doc(groupId);
+    const groupSnapshot = await groupRef.get();
+    const groupData = groupSnapshot.data();
+    let groupMembersId: Array<String> = [];
+    if (groupData!.membersId)
+      groupMembersId = [groupData!.leaderId, ...groupData!.membersId];
+    else groupMembersId = [groupData!.leaderId];
+    const studentsRef = firestore
+      .collection("students")
+      .where(FieldPath.documentId(), "in", groupMembersId);
+    const membersSnapshot = await studentsRef.get();
+    const members: Array<Object> = [];
+    membersSnapshot.forEach((doc) => {
+      const { name, avatarUri } = doc.data();
+      members.push({
+        id: doc.id,
+        name,
+        avatarUri,
+      });
+    });
+    res.status(200).send(members);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
+
+export const addBooking = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const groupId = req.params.id;
+    let notUsedEverytime = [];
+    const { memberId, startTime, endTime, day } = req.body;
+
+    const groupRef = firestore.collection("groups").doc(groupId);
+    const groupSnapshot = await groupRef.get();
+    const memberBookingRef = groupRef.collection("booking");
+    const query = memberBookingRef.where("memberId", "==", memberId);
+    const querySnapshot = await query.get();
+    console.log(groupId);
+
+    if (querySnapshot.empty) {
+      const completeData: any = {
+        memberId,
+        sunday: {},
+        monday: {},
+        tuesday: {},
+        wednesday: {},
+        thursday: {},
+        friday: {},
+        saturday: {},
+        updatedAt: FieldValue.serverTimestamp(),
+      };
+
+      completeData[day].startTime = startTime;
+      completeData[day].endTime = endTime;
+
+      const memberBookingSnapshot = await memberBookingRef.get();
+      if (!memberBookingSnapshot.empty) {
+        memberBookingSnapshot.forEach((doc) => {
+          notUsedEverytime.push(doc.data());
+        });
+      }
+      await memberBookingRef.doc().set(completeData);
+      notUsedEverytime.push(completeData);
+      // const registeredMembersCount = await memberBookingRef
+      //   .get()
+      //   .then((snap) => snap.size);
+      const groupSize = groupSnapshot.data()!.membersId.length + 1;
+
+      const registeredMembersCount = notUsedEverytime.length;
+
+      if (registeredMembersCount === groupSize) {
+        await groupRef.update({
+          confirmedTime: bookingAlgorithm(notUsedEverytime),
+        });
+      } else console.log("so deep");
+      console.log("first");
+      console.log(notUsedEverytime);
+    } else {
+      const dataId: string = querySnapshot.docs[0].id;
+
+      const completeData: any = {
+        updatedAt: FieldValue.serverTimestamp(),
+      };
+      completeData[`${day}.startTime`] = startTime;
+      completeData[`${day}.endTime`] = endTime;
+
+      await memberBookingRef.doc(dataId).update({
+        ...completeData,
+      });
+      console.log("second");
+    }
+    res.status(200).send("hi");
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
+
+export const getBookings = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const groupId = req.params.id;
+    const bookingRef = firestore
+      .collection("groups")
+      .doc(groupId)
+      .collection("booking");
+    const bookingSnapshot = await bookingRef.get();
+    const bookingData: Array<Object> = [];
+    bookingSnapshot.forEach((doc) => {
+      bookingData.push({
+        id: doc.id,
+        ...doc.data(),
+      });
+    });
+
+    res.status(200).send(bookingData);
+  } catch (error) {
+    res.status(400).send(error.message);
   }
 };
--- src/routes/groupRoutes.ts
+++ src/routes/groupRoutes.ts
@@ -1,10 +1,23 @@
 import { Router } from "express";
-import { getGroup, getAllGantt } from "../controllers/groupController";
+import {
+  getGroup,
+  getAllGantt,
+  getGroupMembers,
+  addGroup,
+  addGantt,
+  addBooking,
+  getBookings,
+} from "../controllers/groupController";
 
 const router: Router = Router();
 
 router.get("/group/:id", getGroup);
 router.get("/group/:id/gantt", getAllGantt);
+router.get("/group/:id/members", getGroupMembers);
+router.post("/group/create", addGroup);
+router.post("/group/:id/gantt/create", addGantt);
+router.post("/group/:id/booking", addBooking);
+router.get("/group/:id/booking", getBookings);
 
 export default {
   routes: router,
