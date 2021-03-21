53865911cd35120187600d8a5c985ff364d80f13
data-miner00 <data-miner00@users.noreply.github.com>
2021-03-21 16:35:00 +0800

    fix: Fix wrong routes

--- src/routes/lecturerRoutes.ts
+++ src/routes/lecturerRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getLecturer } from "../controllers/lecturerController";
+import { getLecturer, getSubjects } from "../controllers/lecturerController";
 
 const router: Router = Router();
 
-router.get("lecturer/:id", getLecturer);
+router.get("/lecturer/:id", getLecturer);
+router.get("/lecturer/:id/subjects", getSubjects);
 
 export default {
   routes: router,
