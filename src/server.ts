50a55029eb64a788d0f0bd483d91d9494a2ad862
data-miner00 <data-miner00@users.noreply.github.com>
2021-03-21 17:00:02 +0800

    fix: Removed multer from main file

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import express, { Application, Request, Response, NextFunction } from "express";
 import configs from "./config";
 import cors from "cors";
-import multer from "multer";
 import bodyParser from "body-parser";
 
 import studentRoutes from "./routes/studentRoutes";
@@ -18,19 +17,6 @@ app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 
-// Multer
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, "uploads");
-//   },
-//   filename: (req, file, cb) => {
-//     const { originalname } = file;
-//     cb(null, originalname);
-//   },
-// });
-// app.use(multer({ storage }).array("docs"));
-//
-
 app.use("/api", studentRoutes.routes);
 app.use("/api", credentialRoutes.routes);
 app.use("/api", lecturerRoutes.routes);
@@ -42,9 +28,5 @@ app.use("/api", notificationRoutes.routes);
 app.get("/", async (req: Request, res: Response, next: NextFunction) => {
   res.send("hello");
 });
-
-// app.post("/upload", async (req: Request, res: Response, next: NextFunction) => {
-//   console.dir(req.files);
-// });
 
 app.listen(configs.port, (): void => console.log("Server ready"));
