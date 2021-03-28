4b64b6d8e278962de46285478e5644c644e6830a
data-miner00 <data-miner00@users.noreply.github.com>
2021-03-29 01:29:08 +0800

    feat: Added getSubjects API call

--- src/controllers/subjectController.ts
+++ src/controllers/subjectController.ts
@@ -1,4 +1,4 @@
-import { db as firebase } from "../database";
+import { db as firebase, FieldPath } from "../database";
 import { Request, Response, NextFunction } from "express";
 
 const firestore = firebase.firestore();
@@ -18,6 +18,71 @@ export const getSubject = async (
     } else {
       res.status(404).send("Data not found");
     }
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
+
+/**
+ *  Get a list of subjects.
+ *
+ *  @param {Array<string>} req.body (array of subjectCodes)
+ *
+ */
+export const getSubjects = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const subjectsId = req.body;
+    const subjectsQuery = firestore
+      .collection("subjects")
+      .where(FieldPath.documentId(), "in", subjectsId);
+    const querySnapshot = await subjectsQuery.get();
+
+    type Subject = {
+      code: string;
+      lecturerId: string;
+      assignmentsId: string;
+      description: string;
+      studentsCount: number;
+      name: string;
+    };
+
+    const subjects: Subject[] = [];
+    querySnapshot.forEach(async (doc) => {
+      const {
+        name,
+        lecturerId,
+        assignmentsId,
+        description,
+        studentsCount,
+      } = doc.data()!;
+      subjects.push({
+        code: doc.id,
+        name,
+        lecturerId,
+        assignmentsId,
+        description,
+        studentsCount,
+      });
+    });
+
+    const subjectsV2 = await (async (subjects: Array<Subject>) => {
+      let queries = [];
+      for (let i = 0; i < subjects.length; i++) {
+        const lecturerRef = firestore
+          .collection("lecturer")
+          .doc(subjects[i].lecturerId);
+        const lecturerSnapshot = await lecturerRef.get();
+        const lecturerName = lecturerSnapshot.data()!.name;
+        queries.push({ ...subjects[i], lecturerName });
+      }
+      return Promise.all(queries);
+    })(subjects);
+
+    res.status(200).send(subjectsV2);
   } catch (error) {
     res.status(400).send(error.message);
   }
--- src/routes/subjectRoutes.ts
+++ src/routes/subjectRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getSubject } from "../controllers/subjectController";
+import { getSubject, getSubjects } from "../controllers/subjectController";
 
 const router: Router = Router();
 
 router.get("/subject/:code", getSubject);
+router.post("/subject", getSubjects);
 
 export default {
   routes: router,
