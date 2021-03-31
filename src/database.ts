35d1575515be32eacda1abb398b3333c3a88191f
data-miner00 <data-miner00@users.noreply.github.com>
2021-03-31 17:05:57 +0800

    feat: Added 2 more utils from firebase

--- src/database.ts
+++ src/database.ts
@@ -8,3 +8,7 @@ export const Timestamp = firebase.firestore.Timestamp;
 export const FieldPath = firebase.firestore.FieldPath;
 
 export const FieldValue = firebase.firestore.FieldValue;
+
+export const Increment = firebase.firestore.FieldValue.increment(1);
+
+export const Decrement = firebase.firestore.FieldValue.increment(-1);
