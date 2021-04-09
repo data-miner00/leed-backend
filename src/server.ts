6574d747a0d061ea1b9bcefb755ad32e4dd49a6d
data-miner00 <data-miner00@users.noreply.github.com>
2021-04-10 03:41:45 +0800

    feat: Enable job scheduling

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import express, { Application, Request, Response, NextFunction } from "express";
 import configs from "./config";
+import { db as firebase } from "./database";
 import cors from "cors";
 import bodyParser from "body-parser";
+import schedule from "node-schedule";
 
 import studentRoutes from "./routes/studentRoutes";
 import credentialRoutes from "./routes/credentialRoutes";
@@ -27,6 +29,39 @@ app.use("/api", notificationRoutes.routes);
 
 app.get("/", async (req: Request, res: Response, next: NextFunction) => {
   res.send("hello");
+});
+
+let serverWeek = 12;
+schedule.scheduleJob("0 0 0 * * 1", () => {
+  // Increment week number
+  serverWeek++;
+
+  // Reset discussion time and bookings
+  firebase
+    .firestore()
+    .collection("groups")
+    .get()
+    .then((groups) => {
+      groups.forEach((doc) => {
+        // Reset time
+        doc.ref.update({
+          confirmedTime: {},
+        });
+        // Remove existing bookings
+        doc.ref
+          .collection("booking")
+          .get()
+          .then((bookings) => {
+            bookings.forEach((idoc) => {
+              idoc.ref.delete();
+            });
+          });
+      });
+    });
+});
+
+app.get("/api/week", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ serverWeek });
 });
 
 app.listen(configs.port, (): void => console.log("Server ready"));
